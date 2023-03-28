a7eff1843ca64beb16a4a4942ef9244cee127274
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2023-03-28 17:10:09 +0530

    Added feature to add task to the database
    if the user is adding the data for first time we will create a object with userId and tasks keys in it and add the respective values ,else instead of creating another object with user id we will update only the task by adding more data in the task key only.

--- back-end/controllers/tasks.controller.ts
+++ back-end/controllers/tasks.controller.ts
@@ -1,20 +1,47 @@
-import express from 'express';
-import { TasksList } from '../models/tasks.model';
-import { authenticate } from '../middlewares/authentication';
-import { CustomRequest } from '../middlewares/authentication';
-const router=express.Router();
+import express from "express";
+import { TasksList } from "../models/tasks.model";
+import { authenticate } from "../middlewares/authentication";
+import { CustomRequest } from "../middlewares/authentication";
+const router = express.Router();
 
 //get
-router.get('/',authenticate,async (req:CustomRequest,res)=>{
-    try {
-        const data=await TasksList.find().lean().exec();
-        console.log('user' ,req.user);
-        
-        res.status(200).send(data);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error);
+router.get("/", authenticate, async (req: CustomRequest, res) => {
+  let userId = req.user?.userData._id;
+  try {
+    const data = await TasksList.findOne({ _id: userId }).lean().exec();
+    console.log("my user", req.user?.userData._id);
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+//posting datas of task
+router.post("/", authenticate, async (req: CustomRequest, res) => {
+  let userId = req.user?.userData._id;
+  let postData = {
+    userId: userId,
+    tasks: req.body.tasks,
+  };
+  try {
+    let userTask = await TasksList.findOne({ userId: userId });
+    if (!userTask) {
+      const data = await TasksList.create(postData);
+      console.log("my user", req.user?.userData._id);
+      res.status(200).send(data);
+    } else {
+      const taskDoc = await TasksList.findOneAndUpdate(
+        { userId },
+        { $push: { tasks: req.body.tasks } },
+        { new: true, upsert: true }
+      );
+      res.status(200).send({ message: "task added ", data: taskDoc });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
 
 export default router;
