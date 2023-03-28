0c9a4f60ee190f97fa90b29ea6a4c2c9b75e1cb8
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2023-03-28 12:46:59 +0530

    Added login ,hashing the pasword

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import cors from "cors";
 import { connectMongodb } from "./db/db";
-import userController from './controllers/registration.controller'
+import userController from "./controllers/registration.controller";
 import tasksController from "./controllers/tasks.controller";
+import loginController from "./controllers/login.controller";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(cors());
-
 app.use(express.json());
-
 app.use("/reg", userController);
 app.use("/tasks", tasksController);
+app.use("/log", loginController);
 app.listen(3000, async () => {
   try {
     await connectMongodb();
--- back-end/models/registration.model.ts
+++ back-end/models/registration.model.ts
@@ -1,16 +1,22 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+interface User {
+  email: string;
+  password: string;
+  checkPassword(password: string): boolean;
+}
+
+export interface UserModel extends mongoose.Model<User> {
+  checkPassword(password: string): boolean;
+}
 
 
- interface User {
-    email:string,
-    password:string
- }
-
-export const userSchema = new  mongoose.Schema<User>({
+export const userSchema = new mongoose.Schema<User>({
   email: {
     type: String,
     required: true,
-    unique:true
+    unique: true,
   },
   password: {
     type: String,
@@ -18,4 +24,19 @@ export const userSchema = new  mongoose.Schema<User>({
   },
 });
 
-export const User =mongoose.model('user',userSchema);
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  } else {
+    var hash = bcrypt.hashSync(this.password, 8);
+    this.password = hash;
+
+    return next();
+  }
+});
+
+userSchema.methods.checkPassword = function (password: any) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+export const User = mongoose.model<User,UserModel>("user", userSchema);
