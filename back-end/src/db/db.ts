f4620818e12582516d008214190a5e098bd5bb28
Vishnu PS <95961812+VishnuPScodes@users.noreply.github.com>
2023-03-28 11:54:36 +0530

    Delete back-end/src directory

--- back-end/src/db/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-
-export const connectMongodb =()=>{
- return mongoose.connect(
-   "mongodb+srv://psvishnu373:vishnu@cluster0.dyftydh.mongodb.net/?retryWrites=true&w=majority"
- );
- }
